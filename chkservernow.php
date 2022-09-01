769f6a00b7b45bf781085d7683808d0ab7ba55ee
THONGTEW19dev <110838149+THONGTEW19dev@users.noreply.github.com>
2022-09-02 01:22:35 +0700

    Add files via upload

---
+++ chkservernow.php
@@ -0,0 +1,9 @@
+<?php
+    $urlfile = $_SERVER['PHP_SELF'];
+    echo $urlfile . "<br>";
+    #ChkServerNow/chkservernow.php
+
+    $urlfilenow = basename($_SERVER['PHP_SELF']);
+    echo $urlfilenow;
+    #chkservernow.php
+?>
